d8fece31f0ec234ebc84791f5b731ab2225c604c
YRHwang90 <70814226+YRHwang90@users.noreply.github.com>
2023-11-22 16:56:06 +0900

    1

--- manuscripts/scripts/gen_rmvn.R
+++
@@ -1,4 +0,0 @@
-rmvn <- function(n,sigma) {
-  Sh <- with(svd(sigma), v%*%diag(sqrt(d))%*%t(u))
-  matrix(stats::rnorm(ncol(sigma)*n),ncol=ncol(sigma))%*%Sh
-}
--- manuscripts/scripts/model_null.R
+++
@@ -1,5 +0,0 @@
-## the function to run null model
-
-model_null <- function(data){
-  lmer(y~1+(1|pid), data=data)
-}
